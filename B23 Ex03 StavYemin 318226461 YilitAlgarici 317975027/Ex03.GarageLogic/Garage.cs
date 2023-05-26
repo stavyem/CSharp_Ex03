76c62485c290366dd43473f5562b0a0930c89a7e
Stav Yemin <stavy402@gmail.com>
2023-05-26 18:53:06 +0300

    wrote comments on push command

---
+++ B23 Ex03 StavYemin 318226461 YilitAlgarici 317975027/Ex03.GarageLogic/Garage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal class Garage
+    {
+        // work with separated branches.
+        // every big change (end day, finish class, finish function) --> do push
+        // 
+    }
+}

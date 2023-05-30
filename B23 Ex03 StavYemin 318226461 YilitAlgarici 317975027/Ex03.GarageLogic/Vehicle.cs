0cf55747d304d9573ea1335bc29016350850db98
Stav Yemin <stavy402@gmail.com>
2023-05-30 14:34:25 +0300

    created Vehicle class

---
+++ B23 Ex03 StavYemin 318226461 YilitAlgarici 317975027/Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    internal abstract class Vehicle
+    {
+        private string m_Model;
+        private string m_LicenceNum;
+        private float m_EnergyPercentage;
+        private list<wheel> m_WheelCollection;
+        private EnergySource m_EnergySource;
+
+
+    }
+}
